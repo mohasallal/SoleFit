20868e942b336961683beb0552e3cc449a29d8cb
Mohammad Alsallal <malslal787@gmail.com>
2024-03-23 00:54:12 +0300

    fixedGridLayout
    i fixed the grid positioning and centered it in the page and working on the dropdown list rn

---
+++ WebFiles/index.js
@@ -0,0 +1,9 @@
+const list = document.getElementById("dropdownlist");
+
+function clickfunction() {
+  if (list.style.display === "none") {
+    list.style.display = "flex";
+  } else {
+    "dropdownlist".style.display = "none";
+  }
+}
