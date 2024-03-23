3cabd94f329b5c4012d891032bc2982889d68e75
Mohammad Alsallal <malslal787@gmail.com>
2024-03-23 09:49:11 +0300

    fixed the dropdown list
    fixed the drop down list and added action to the button in the header

--- WebFiles/index.js
+++ WebFiles/index.js
@@ -1,9 +1,53 @@
-const list = document.getElementById("dropdownlist");
+document.addEventListener("DOMContentLoaded", function () {
+  var link = document.getElementById("mnyvrsnclk");
+  var dropdown = document.querySelector(".dropdownlistTwo");
 
-function clickfunction() {
-  if (list.style.display === "none") {
-    list.style.display = "flex";
-  } else {
-    "dropdownlist".style.display = "none";
-  }
-}
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (dropdown.style.display === "none") {
+      dropdown.style.display = "flex";
+    } else {
+      dropdown.style.display = "none";
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      event.target !== link &&
+      event.target.closest(".dropdownlistTwo") !== dropdown
+    ) {
+      dropdown.style.display = "none";
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var link = document.getElementById("wrkflows");
+  var dropdown = document.querySelector(".dropdownlistOne");
+
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (dropdown.style.display === "none") {
+      dropdown.style.display = "flex";
+    } else {
+      dropdown.style.display = "none";
+    }
+  });
+  document.addEventListener("click", function (event) {
+    if (
+      event.target !== link &&
+      event.target.closest(".dropdownlistOne") !== dropdown
+    ) {
+      dropdown.style.display = "none";
+    }
+  });
+});
+document.getElementById("headerbutton").addEventListener("click", function () {
+  var scrollPosition = window.innerHeight * 0.8;
+
+  window.scrollTo({
+    top: scrollPosition,
+    behavior: "smooth",
+  });
+});
